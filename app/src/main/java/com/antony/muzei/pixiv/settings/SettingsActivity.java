35a438f1eae99abce49cfdc151207db9b4063572
Antony <yellowbluesky1@gmail.com>
2019-07-04 22:02:56 +1000

    reversion

--- app/src/main/java/com/antony/muzei/pixiv/settings/SettingsActivity.java
+++ app/src/main/java/com/antony/muzei/pixiv/settings/SettingsActivity.java
@@ -8,23 +8,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
-import androidx.work.Constraints;
-import androidx.work.ExistingPeriodicWorkPolicy;
-import androidx.work.NetworkType;
-import androidx.work.PeriodicWorkRequest;
-import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
 
 import com.antony.muzei.pixiv.PixivArtProvider;
 import com.antony.muzei.pixiv.R;
 import com.google.android.apps.muzei.api.provider.Artwork;
 import com.google.android.apps.muzei.api.provider.ProviderContract;
-import com.google.common.util.concurrent.ListenableFuture;
-
-import java.util.Calendar;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
 
 public class SettingsActivity extends AppCompatActivity
 {
@@ -60,28 +49,28 @@ public class SettingsActivity extends AppCompatActivity
             @Override
             public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key)
             {
-                switch (key)
-                {
-                    case "pref_loginPassword":
-                        newCreds = sharedPrefs.getString("pref_loginPassword", "");
-                        break;
-                    case "pref_updateMode":
-                        newUpdateMode = sharedPrefs.getString("oldUpdateMode", "");
-                        break;
-                    case "pref_nsfwFilterLevel":
-                        newFilter = sharedPrefs.getString("pref_nsfwFilterLevel", "");
-                        break;
-                }
-//                if (key.equals("pref_loginPassword"))
+//                switch (key)
 //                {
-//                    newCreds = sharedPrefs.getString("pref_loginPassword", "");
-//                } else if (key.equals("pref_updateMode"))
-//                {
-//                    newUpdateMode = sharedPrefs.getString("oldUpdateMode", "");
-//                } else if (key.equals("pref_nsfwFilterLevel"))
-//                {
-//                    newFilter = sharedPrefs.getString("pref_nsfwFilterLevel", "");
+//                    case "pref_loginPassword":
+//                        newCreds = sharedPrefs.getString("pref_loginPassword", "");
+//                        break;
+//                    case "pref_updateMode":
+//                        newUpdateMode = sharedPrefs.getString("oldUpdateMode", "");
+//                        break;
+//                    case "pref_nsfwFilterLevel":
+//                        newFilter = sharedPrefs.getString("pref_nsfwFilterLevel", "");
+//                        break;
 //                }
+                if (key.equals("pref_loginPassword"))
+                {
+                    newCreds = sharedPrefs.getString("pref_loginPassword", "");
+                } else if (key.equals("pref_updateMode"))
+                {
+                    newUpdateMode = sharedPrefs.getString("oldUpdateMode", "");
+                } else if (key.equals("pref_nsfwFilterLevel"))
+                {
+                    newFilter = sharedPrefs.getString("pref_nsfwFilterLevel", "");
+                }
             }
         };
     }
@@ -127,51 +116,16 @@ public class SettingsActivity extends AppCompatActivity
 
         if (!oldUpdateMode.equals(newUpdateMode))
         {
-            WorkManager manager = WorkManager.getInstance();
-            manager.cancelAllWorkByTag("PIXIV");
+            //WorkManager manager = WorkManager.getInstance();
+            //manager.cancelAllWorkByTag("PIXIV");
             ProviderContract.getProviderClient(getApplicationContext(), PixivArtProvider.class).setArtwork(new Artwork());
             Toast.makeText(getApplicationContext(), getString(R.string.toast_newUpdateMode), Toast.LENGTH_SHORT).show();
         } else if (!oldFilter.equals(newFilter))
         {
-            WorkManager manager = WorkManager.getInstance();
-            manager.cancelAllWorkByTag("PIXIV");
+            //WorkManager manager = WorkManager.getInstance();
+            //manager.cancelAllWorkByTag("PIXIV");
             ProviderContract.getProviderClient(getApplicationContext(), PixivArtProvider.class).setArtwork(new Artwork());
             Toast.makeText(getApplicationContext(), getString(R.string.toast_newFilterMode), Toast.LENGTH_SHORT).show();
-        }
-        if (PreferenceManager.getDefaultSharedPreferences(
-                getApplicationContext()).getBoolean("pref_autoClearMode", false))
-        {
-            WorkManager manager = WorkManager.getInstance(getApplicationContext());
-            ListenableFuture<List<WorkInfo>> future = manager.getWorkInfosByTag("PIXIV_CACHECLEAR");
-            try
-            {
-                List<WorkInfo> list = future.get();
-                if ((list == null) || (list.size() == 0))
-                {
-                    // Calculating the number of hours until 4AM
-                    Calendar timeNow = Calendar.getInstance();
-                    int hoursNow = timeNow.get(Calendar.HOUR_OF_DAY);
-                    int hoursUntilFour = hoursNow > 23 ? 4 - hoursNow : hoursNow - 4;
-
-                    Constraints constraints = new Constraints.Builder()
-                            .setRequiredNetworkType(NetworkType.CONNECTED)
-                            .build();
-                    PeriodicWorkRequest request = new PeriodicWorkRequest.Builder(ClearCacheWorker.class, 24, TimeUnit.HOURS)
-                            .setInitialDelay(hoursUntilFour, TimeUnit.HOURS)
-                            .addTag("PIXIV_CACHECLEAR")
-                            .setConstraints(constraints)
-                            .build();
-                    manager.enqueueUniquePeriodicWork("PIXIV_CACHECLEAR", ExistingPeriodicWorkPolicy.KEEP, request);
-                    }
-            } catch (InterruptedException | ExecutionException ex)
-            {
-                ex.printStackTrace();
-            }
-            Toast.makeText(getApplicationContext(), "Automatically clearing cache", Toast.LENGTH_SHORT).show();
-        } else
-        {
-            WorkManager.getInstance(getApplicationContext()).cancelAllWorkByTag("PIXIV_CACHECLEAR");
-            Toast.makeText(getApplicationContext(), "Not automatically clearing cache", Toast.LENGTH_SHORT).show();
         }
     }
 
