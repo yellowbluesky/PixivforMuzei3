48c36ce938429a7e6349c00be12c01943231900e
Antony <yellowbluesky1@gmail.com>
2019-08-09 20:27:36 +1000

    added license

--- app/src/main/java/com/antony/muzei/pixiv/ClearCacheWorker.java
+++ app/src/main/java/com/antony/muzei/pixiv/ClearCacheWorker.java
@@ -1,15 +1,28 @@
+/*
+    This file is part of PixivforMuzei3.
+
+    PixivforMuzei3 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program  is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package com.antony.muzei.pixiv;
 
-import android.content.ContentResolver;
 import android.content.Context;
-import android.net.Uri;
 import android.os.Environment;
 
 import androidx.annotation.NonNull;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
-
-import com.google.android.apps.muzei.api.provider.ProviderContract;
 
 import org.apache.commons.io.FileUtils;
 
