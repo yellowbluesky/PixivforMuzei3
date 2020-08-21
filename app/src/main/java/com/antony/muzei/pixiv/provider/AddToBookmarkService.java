4a81cc4b0c8a4daafc79b8ebc4ae8af234c98ef9
Antony Hu <yellowbluesky1@gmail.com>
2020-08-21 20:38:24 +1000

    Add to bookmark command works on Android 9 or lower

--- app/src/main/java/com/antony/muzei/pixiv/provider/AddToBookmarkService.java
+++ app/src/main/java/com/antony/muzei/pixiv/provider/AddToBookmarkService.java
@@ -21,6 +21,7 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.JobIntentService;
@@ -28,31 +29,57 @@ import androidx.core.app.NotificationCompat;
 
 import com.antony.muzei.pixiv.R;
 
-public class AddToBookmarkService extends JobIntentService
-{
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.FormBody;
+import okhttp3.HttpUrl;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class AddToBookmarkService extends JobIntentService {
     final Handler mHandler = new Handler();
 
-    private static final String TAG = "MyJobIntentService";
+    private static final String TAG = "AddToBookmarkService";
     /**
      * Unique job ID for this service.
      */
     private static final int JOB_ID = 2;
+    OkHttpClient client = new OkHttpClient();
+    String BASE_URL = "https://ptsv2.com/";
+
     public static void enqueueWork(Context context, Intent intent) {
         enqueueWork(context, AddToBookmarkService.class, JOB_ID, intent);
     }
 
     @Override
-    protected void onHandleWork(@NonNull Intent intent)
-    {
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.muzei_launch_command)
-                .setContentTitle("My notification")
-                .setContentText("Hello World!");
-
-        NotificationManager mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-// mId allows you to update the notification later on.
-        mNotificationManager.notify(1, mBuilder.build());
+    protected void onHandleWork(@NonNull Intent intent) {
+        // TODO use RestClient
+        HttpUrl rankingUrl = new HttpUrl.Builder()
+                .scheme("https")
+                .host("app-api.pixiv.net")
+                .addPathSegments("v2/illust/bookmark/add")
+                .build();
+        RequestBody authData = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("illust_id", intent.getStringExtra("artworkId"))
+                .addFormDataPart("restrict", "public")
+                .build();
+        Request request = new Request.Builder()
+                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                .addHeader("User-Agent", PixivArtProviderDefines.APP_USER_AGENT)
+                .addHeader("Authorization", "Bearer " + intent.getStringExtra("accessToken"))
+                .post(authData)
+                .url(rankingUrl)
+                .build();
+        try {
+            client.newCall(request).execute();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
     @Override
