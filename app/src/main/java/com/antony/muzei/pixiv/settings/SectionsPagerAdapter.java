59a3834fb77a71642e0ac112783e2a87cc82781a
Antony Hu <yellowbluesky1@gmail.com>
2021-07-07 22:02:57 +1000

    Replaced deprecated FragmentPagerDapter with FragmentStateAdapter

--- app/src/main/java/com/antony/muzei/pixiv/settings/SectionsPagerAdapter.java
+++ app/src/main/java/com/antony/muzei/pixiv/settings/SectionsPagerAdapter.java
@@ -17,43 +17,29 @@
 
 package com.antony.muzei.pixiv.settings;
 
-import android.content.Context;
-
-import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
-import com.antony.muzei.pixiv.R;
-import com.antony.muzei.pixiv.settings.fragments.AdvOptionsPreferenceFragment;
 import com.antony.muzei.pixiv.settings.deleteArtwork.ArtworkDeletionFragment;
+import com.antony.muzei.pixiv.settings.fragments.AdvOptionsPreferenceFragment;
 import com.antony.muzei.pixiv.settings.fragments.CreditsPreferenceFragment;
 import com.antony.muzei.pixiv.settings.fragments.MainPreferenceFragment;
 import com.antony.muzei.pixiv.settings.fragments.RoadmapPreferenceFragment;
 
-/**
- * A [FragmentPagerAdapter] that returns a fragment corresponding to
- * one of the sections/tabs/pages.
- */
-public class SectionsPagerAdapter extends FragmentPagerAdapter
-{
+import org.jetbrains.annotations.NotNull;
 
-    @StringRes
-    private static final int[] TAB_TITLES = new int[]{R.string.tab_heading_main, R.string.tab_heading_adv_options, R.string.tab_heading_artwork_delete, R.string.tab_heading_credits, R.string.tab_heading_roadmap};
-    private final Context mContext;
-
-    public SectionsPagerAdapter(Context context, FragmentManager fm)
-    {
-        super(fm, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
-        mContext = context;
+public class SectionsPagerAdapter extends FragmentStateAdapter {
+    public SectionsPagerAdapter(FragmentActivity fa) {
+        super(fa);
     }
 
+    @NonNull
+    @NotNull
     @Override
-    public Fragment getItem(int position)
-    {
-        switch (position)
-        {
+    public Fragment createFragment(int position) {
+        switch (position) {
             case 0:
                 return new MainPreferenceFragment();
             case 1:
@@ -68,17 +54,8 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter
         }
     }
 
-    @Nullable
     @Override
-    public CharSequence getPageTitle(int position)
-    {
-        return mContext.getResources().getString(TAB_TITLES[position]);
-    }
-
-    @Override
-    public int getCount()
-    {
-        // How many pages to show
+    public int getItemCount() {
         return 5;
     }
 }
