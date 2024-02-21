18e01d67296cf9ec3116b5a839fe3eb8389b1265
antony <antony@antonyhu,cin,au>
2024-02-21 20:42:06 +0400

    Cleaned imports

--- app/src/main/java/com/antony/muzei/pixiv/provider/network/interceptor/StandardAuthHttpHeaderInterceptor.kt
+++ app/src/main/java/com/antony/muzei/pixiv/provider/network/interceptor/StandardAuthHttpHeaderInterceptor.kt
@@ -1,14 +1,9 @@
 package com.antony.muzei.pixiv.provider.network.interceptor
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.icu.text.SimpleDateFormat
 import android.os.Build
-import android.util.Log
-import com.antony.muzei.pixiv.PixivInstrumentation.Companion.INTENT_ACTION_ACCESS_TOKEN_MISSING
-import com.antony.muzei.pixiv.PixivMuzeiSupervisor
 import com.antony.muzei.pixiv.PixivProviderConst.APP_USER_AGENT
-import com.antony.muzei.pixiv.provider.exceptions.AccessTokenAcquisitionException
 import okhttp3.Interceptor
 import okhttp3.Response
 import java.math.BigInteger
@@ -29,10 +24,12 @@ class StandardAuthHttpHeaderInterceptor : Interceptor {
         } else {
             ""
         }
-        val intermediateString = formattedTime + "28c1fdd170a5204386cb1313c7077b34f83e4aaf4aa829ce78c231e05b0bae2c"
+        val intermediateString =
+            formattedTime + "28c1fdd170a5204386cb1313c7077b34f83e4aaf4aa829ce78c231e05b0bae2c"
 
         val md = MessageDigest.getInstance("MD5")
-        val hash = BigInteger(1, md.digest(intermediateString.toByteArray())).toString(16).padStart(32, '0')
+        val hash = BigInteger(1, md.digest(intermediateString.toByteArray())).toString(16)
+            .padStart(32, '0')
 
         val newRequest = originRequest.newBuilder()
             //.header("authorization", "Bearer $token")
